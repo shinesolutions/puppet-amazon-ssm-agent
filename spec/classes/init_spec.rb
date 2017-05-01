8731bc7cd14e8d827c3d5d1307c7252aadcd70ad
Andy Wang <andy.wang@shinesolutions.com>
2017-05-01 13:20:26 +1000

    start adding test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
-describe 'amz_ssm_agent' do
-  context 'with default values for all parameters' do
-    it { should contain_class('amz_ssm_agent') }
+describe 'amazon_ssm_agent' do
+  context 'On RedHat with default values for all parameters' do
+    let (:facts ) {
+      {
+        :os => {
+          :architecture => "x86_64",
+          :family => "RedHat",
+          :hardware => "x86_64",
+          :name => "Darwin",
+          :release => {
+            :full => "16.5.0",
+            :major => "16",
+            :minor => "5"
+          }
+        }
+      }
+    }
+
+    it { should contain_class('amazon_ssm_agent') }
   end
 end
